0aa1dd8e4546d1ed485a6cc10fe315df0acc007c
Eugene Vakarchuk <eugenevakarchuk@gmail.com>
2023-06-03 01:28:06 +0300

    created the functionality of cleaning the cart when changing goods

--- client/src/components/GoodItem.tsx
+++ client/src/components/GoodItem.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import {IGood} from "../models/IGood";
+import GoodItemUI from "../ui/GoodItemUI";
+import CartService from "../services/CartService";
 
 const GoodItem: React.FC<IGood> = (props) => {
+	const goodId = props._id;
+	const cartId = localStorage.getItem("cartId");
+	const [goodAddedtoCart, setGoodAddedToCart] = useState(false);
+
+	const addGoodToCart = async () => {
+		console.log(goodId);
+		console.log(cartId);
+		const updatedCart = await CartService.addGoodToCart(goodId, cartId);
+		console.log(updatedCart);
+	};
+
 	return (
 		<div>
-			<h3>{props.goodTitle}</h3>
-			<p>{props.goodPrice}</p>
+			<GoodItemUI title={props.goodTitle} price={props.goodPrice} />
+			<button onClick={addGoodToCart}>Add to cart</button>
 		</div>
 	);
 };
